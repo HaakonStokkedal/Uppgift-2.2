9306a335aedbe30a20dfcde663f7c2b1100b64a5
hast06001 <hast06001@E-PW024NW1>
2023-09-19 21:40:44 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift_2._2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Elin och Alma hoppade på i en längdhoppstävling men Elin hoppade längst.");
+            Console.WriteLine("Hur långt hoppade Elin? (i meter)");
+            string elinText = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade Alma? (i meter)");
+            string almaText = Console.ReadLine();
+            double elin = double.Parse(elinText);
+            double alma = double.Parse(almaText);
+            double elinLängre = elin - alma;
+            Console.WriteLine("Elin hoppade " + elinLängre + " meter längre än Alma.,");
+
+            Console.ReadKey();
+        }
+    }
+}
